a75541f90582c3a315d502b82bb68d294532fe2f
Shane Glass <shane@shattered.dev>
2023-01-02 17:01:45 +1000

    CLEAN CREDS

--- utils/db/mongo.ts
+++
@@ -1,8 +0,0 @@
-// import mongoose from "mongoose";
-
-
-// const MONGODB_URI = "mongodb+srv://shattered_site_admin:gthKlI23354bnmlkrujkl@shattered.hd4ne8b.mongodb.net/?retryWrites=true&w=majority";
-// // 'mongodb://shattered_site_admin:gthKlI23354bnmlkrujkl@ac-qdpxluq-shard-00-00.hd4ne8b.mongodb.net:27017,ac-qdpxluq-shard-00-01.hd4ne8b.mongodb.net:27017,ac-qdpxluq-shard-00-02.hd4ne8b.mongodb.net:27017/?ssl=true&replicaSet=atlas-co3nft-shard-0&authSource=admin&retryWrites=true&w=majority';
-
-// export default mongoose
-//     .connect(MONGODB_URI);
